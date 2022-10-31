2c42f4a286d4c6d85bb747b5b4851e0debe3614e
Chris Elston <chris.elston@blueyonder.co.uk>
2022-10-31 14:40:25 +0000

    Allow connections from localhost:3000

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express, { Express, Router } from 'express';
+import cors from 'cors';
 
 export const createExpressApp = (galleryRouter: Router): Express => {
     const app = express();
+    const allowedOrigins = ['http://localhost:3000'];
 
+    const options: cors.CorsOptions = { origin: allowedOrigins };
+    app.use(cors(options));
     app.use('/gallery', galleryRouter);
 
     return app;

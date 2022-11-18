910080e0031243f291c389e5e73b723b99f8fb72
Chris Elston <chris.elston@blueyonder.co.uk>
2022-11-18 22:24:49 +0000

    Markdown: Remove extension from GET endpoint
    Search for either path.md or path/index.md and fail
    if neither are found
    
    Resolves #25

--- src/handlers/getMarkdownFileHandler.ts
+++ src/handlers/getMarkdownFileHandler.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from 'express';
 import path from 'path';
+import fs from 'fs';
 import { RequestHandler } from './RequestHandler';
 
 export const createGetMarkdownFileHandler = (): RequestHandler => async (req: Request, res: Response) => {
     const { mdPath } = req.params;
     const contenDir = '/home/chris/coding/javascript/home-api/content/markdown/';
-    const filePath = path.resolve(contenDir, mdPath);
+    let filePath = path.resolve(contenDir, `${mdPath}.md`);
+    if (!fs.existsSync(filePath)) {
+        filePath = path.resolve(contenDir, mdPath, 'index.md');
+    }
     try {
         res.sendFile(filePath);
     } catch {

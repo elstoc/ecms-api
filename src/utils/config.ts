42390c3fb4bf26847fd10bcb666b2ac9fc107c56
Chris Elston <chris.elston@blueyonder.co.uk>
2023-01-05 20:23:24 +0000

    Add config for jwt

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,17 +1,29 @@
 export type Config = {
+    port: number;
+    uiSiteUrl: string;
     contentDir: string;
     cacheDir: string;
     adminDir: string;
-    port: number;
-    uiSiteUrl: string;
+    jwtIssuer: string;
+    jwtAudience: string;
+    jwtRefreshExpires: string;
+    jwtAccessExpires: string;
+    jwtRefreshSecret: string;
+    jwtAccessSecret: string;
 };
 
 export const getConfig = (): Config => {
     return {
+        port: parseInt(process.env.PORT || '3123'),
+        uiSiteUrl: process.env.UI_SITE_URL || '',
         contentDir: process.env.CONTENT_DIR || '',
         cacheDir: process.env.CACHE_DIR || '',
         adminDir: process.env.ADMIN_DIR || '',
-        port: parseInt(process.env.PORT || '3123'),
-        uiSiteUrl: process.env.UI_SITE_URL || '',
+        jwtIssuer: process.env.JWT_ISSUER || '',
+        jwtAudience: process.env.JWT_AUDIENCE || '',
+        jwtRefreshExpires: process.env.JWT_REFRESH_EXPIRES || '',
+        jwtAccessExpires: process.env.JWT_ACCESS_EXPIRES || '',
+        jwtRefreshSecret: process.env.JWT_REFRESH_SECRET || '',
+        jwtAccessSecret: process.env.JWT_ACCESS_SECRET || '',
     };
 };

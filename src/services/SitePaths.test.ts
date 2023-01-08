ca33b931ad243bec7dbadd0d1c314500275d940f
Chris Elston <chris.elston@blueyonder.co.uk>
2023-01-08 23:20:17 +0000

    Amend SitePaths to handle admin path

--- src/services/SitePaths.test.ts
+++ src/services/SitePaths.test.ts
@@ -6,83 +6,116 @@ jest.mock('fs');
 
 const config = {
     cacheDir: '/path/to/cache',
-    contentDir: '/path/to/content'
+    contentDir: '/path/to/content',
+    adminDir: '/path/to/admin'
 } as any;
 
-describe('That SitePaths.getCachePath', () => {
-    it('attempts to resolve a cache path correctly', () => {
-        const expectedResult = '/path/to/cache/path/to/required/file';
-        const sitePaths = new SitePaths(config);
-        const cachePath = sitePaths.getCachePath('path', 'to', 'required', 'file');
-        expect(cachePath).toBe(expectedResult);
+describe('That SitePaths', () => {
+    let sitePaths: SitePaths;
+
+    beforeEach(() => {
+        sitePaths = new SitePaths(config);
     });
 
-    it('returns the base cache path when called with no parameters', () => {
-        const expectedResult = '/path/to/cache';
-        const sitePaths = new SitePaths(config);
-        const cachePath = sitePaths.getCachePath();
-        expect(cachePath).toBe(expectedResult);
+    describe('getContentPath', () => {
+        it('attempts to resolve a content path correctly', () => {
+            const expectedResult = '/path/to/content/path/to/required/file';
+            const contentPath = sitePaths.getContentPath('path', 'to', 'required', 'file');
+            expect(contentPath).toBe(expectedResult);
+        });
+
+        it('returns the base content path when called with no parameters', () => {
+            const expectedResult = '/path/to/content';
+            const contentPath = sitePaths.getContentPath();
+            expect(contentPath).toBe(expectedResult);
+        });
+
+        it('returns the base content path when called with empty string', () => {
+            const expectedResult = '/path/to/content';
+            const contentPath = sitePaths.getContentPath('');
+            expect(contentPath).toBe(expectedResult);
+        });
     });
 
-    it('returns the base cache path when called with empty string', () => {
-        const expectedResult = '/path/to/cache';
-        const sitePaths = new SitePaths(config);
-        const cachePath = sitePaths.getCachePath('');
-        expect(cachePath).toBe(expectedResult);
-    });
-});
+    describe('getCachePath', () => {
+        it('attempts to resolve a cache path correctly', () => {
+            const expectedResult = '/path/to/cache/path/to/required/file';
+            const cachePath = sitePaths.getCachePath('path', 'to', 'required', 'file');
+            expect(cachePath).toBe(expectedResult);
+        });
 
-describe('That SitePaths.getContentPath', () => {
-    it('attempts to resolve a content path correctly', () => {
-        const expectedResult = '/path/to/content/path/to/required/file';
-        const sitePaths = new SitePaths(config);
-        const contentPath = sitePaths.getContentPath('path', 'to', 'required', 'file');
-        expect(contentPath).toBe(expectedResult);
-    });
+        it('returns the base cache path when called with no parameters', () => {
+            const expectedResult = '/path/to/cache';
+            const cachePath = sitePaths.getCachePath();
+            expect(cachePath).toBe(expectedResult);
+        });
 
-    it('returns the base content path when called with no parameters', () => {
-        const expectedResult = '/path/to/content';
-        const sitePaths = new SitePaths(config);
-        const contentPath = sitePaths.getContentPath();
-        expect(contentPath).toBe(expectedResult);
+        it('returns the base cache path when called with empty string', () => {
+            const expectedResult = '/path/to/cache';
+            const cachePath = sitePaths.getCachePath('');
+            expect(cachePath).toBe(expectedResult);
+        });
     });
 
-    it('returns the base content path when called with empty string', () => {
-        const expectedResult = '/path/to/content';
-        const sitePaths = new SitePaths(config);
-        const contentPath = sitePaths.getContentPath('');
-        expect(contentPath).toBe(expectedResult);
-    });
-});
+    describe('getAdminPath', () => {
+        it('attempts to resolve an admin path correctly', () => {
+            const expectedResult = '/path/to/admin/path/to/required/file';
+            const adminPath = sitePaths.getAdminPath('path', 'to', 'required', 'file');
+            expect(adminPath).toBe(expectedResult);
+        });
 
-describe('That SitePaths.getContentPathIfExists', () => {
-    it('returns the content path if it exists', () => {
-        const expectedResult = '/path/to/content';
-        jest.spyOn(fs, 'existsSync').mockReturnValue(true);
-        const sitePaths = new SitePaths(config);
-        const contentPath = sitePaths.getContentPathIfExists('');
-        expect(contentPath).toBe(expectedResult);
-    });
+        it('returns the base admin path when called with no parameters', () => {
+            const expectedResult = '/path/to/admin';
+            const adminPath = sitePaths.getAdminPath();
+            expect(adminPath).toBe(expectedResult);
+        });
 
-    it('throws an error if path does not exist', () => {
-        jest.spyOn(fs, 'existsSync').mockReturnValue(false);
-        const sitePaths = new SitePaths(config);
-        expect(() => sitePaths.getContentPathIfExists('')).toThrow('File "/path/to/content" does not exist');
-    });
-});
-
-describe('That SitePaths.getCachePathIfExists', () => {
-    it('returns the cache path if it exists', () => {
-        const expectedResult = '/path/to/cache';
-        jest.spyOn(fs, 'existsSync').mockReturnValue(true);
-        const sitePaths = new SitePaths(config);
-        const cachePath = sitePaths.getCachePathIfExists('');
-        expect(cachePath).toBe(expectedResult);
+        it('returns the base cache path when called with empty string', () => {
+            const expectedResult = '/path/to/admin';
+            const adminPath = sitePaths.getAdminPath('');
+            expect(adminPath).toBe(expectedResult);
+        });
     });
 
-    it('throws an error if path does not exist', () => {
-        jest.spyOn(fs, 'existsSync').mockReturnValue(false);
-        const sitePaths = new SitePaths(config);
-        expect(() => sitePaths.getCachePathIfExists('')).toThrow('File "/path/to/cache" does not exist');
+    describe('getContentPathIfExists', () => {
+        it('returns the content path if it exists', () => {
+            const expectedResult = '/path/to/content';
+            jest.spyOn(fs, 'existsSync').mockReturnValue(true);
+            const contentPath = sitePaths.getContentPathIfExists('');
+            expect(contentPath).toBe(expectedResult);
+        });
+
+        it('throws an error if path does not exist', () => {
+            jest.spyOn(fs, 'existsSync').mockReturnValue(false);
+            expect(() => sitePaths.getContentPathIfExists('')).toThrow('File "/path/to/content" does not exist');
+        });
+    });
+
+    describe('getCachePathIfExists', () => {
+        it('returns the cache path if it exists', () => {
+            const expectedResult = '/path/to/cache';
+            jest.spyOn(fs, 'existsSync').mockReturnValue(true);
+            const cachePath = sitePaths.getCachePathIfExists('');
+            expect(cachePath).toBe(expectedResult);
+        });
+
+        it('throws an error if path does not exist', () => {
+            jest.spyOn(fs, 'existsSync').mockReturnValue(false);
+            expect(() => sitePaths.getCachePathIfExists('')).toThrow('File "/path/to/cache" does not exist');
+        });
+    });
+
+    describe('getAdminPathIfExists', () => {
+        it('returns the admin path if it exists', () => {
+            const expectedResult = '/path/to/admin';
+            jest.spyOn(fs, 'existsSync').mockReturnValue(true);
+            const adminPath = sitePaths.getAdminPathIfExists('');
+            expect(adminPath).toBe(expectedResult);
+        });
+
+        it('throws an error if path does not exist', () => {
+            jest.spyOn(fs, 'existsSync').mockReturnValue(false);
+            expect(() => sitePaths.getAdminPathIfExists('')).toThrow('File "/path/to/admin" does not exist');
+        });
     });
 });
--- src/services/SitePaths.ts
+++ src/services/SitePaths.ts
@@ -5,10 +5,21 @@ import { Config } from '../utils';
 export class SitePaths {
     private contentDir: string;
     private cacheDir: string;
+    private adminDir: string;
 
     public constructor(config: Config) {
         this.contentDir = config.contentDir;
         this.cacheDir = config.cacheDir;
+        this.adminDir = config.adminDir;
+    }
+
+    public getContentPath(...sitePaths: string[]): string {
+        return path.resolve(this.contentDir, ...sitePaths);
+    }
+
+    public getContentPathIfExists(...sitePaths: string[]): string {
+        const fullPath = this.getContentPath(...sitePaths);
+        return this.getPathIfExists(fullPath);
     }
 
     public getCachePath(...sitePaths: string[]): string {
@@ -20,19 +31,19 @@ export class SitePaths {
         return this.getPathIfExists(fullPath);
     }
     
+    public getAdminPath(...sitePaths: string[]): string {
+        return path.resolve(this.adminDir, ...sitePaths);
+    }
+
+    public getAdminPathIfExists(...sitePaths: string[]): string {
+        const fullPath = this.getAdminPath(...sitePaths);
+        return this.getPathIfExists(fullPath);
+    }
+    
     private getPathIfExists(fullPath: string): string {
         if (!fs.existsSync(fullPath)) {
             throw new Error(`File "${fullPath}" does not exist`);
         }
         return fullPath;
-    }
-
-    public getContentPath(...sitePaths: string[]): string {
-        return path.resolve(this.contentDir, ...sitePaths);
-    }
-
-    public getContentPathIfExists(...sitePaths: string[]): string {
-        const fullPath = this.getContentPath(...sitePaths);
-        return this.getPathIfExists(fullPath);
     }
 }

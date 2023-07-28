255b92234ef7f760d695f31e33c59f9dea8cbe49
Chris Elston <chris.elston@blueyonder.co.uk>
2023-07-28 21:44:15 +0100

    use nullish coalescing operator

--- src/utils/site/config.ts
+++ src/utils/site/config.ts
@@ -13,15 +13,15 @@ export type Config = {
 
 export const getConfig = (): Config => {
     return {
-        url: process.env.URL || 'http://localhost:3123',
-        port: parseInt(process.env.PORT || '3123'),
-        uiUrl: process.env.UI_URL || '',
-        dataDir: process.env.DATA_DIR || '',
-        jwtIssuer: process.env.JWT_ISSUER || '',
-        jwtAudience: process.env.JWT_AUDIENCE || '',
-        jwtRefreshExpires: process.env.JWT_REFRESH_EXPIRES || '',
-        jwtAccessExpires: process.env.JWT_ACCESS_EXPIRES || '',
-        jwtRefreshSecret: process.env.JWT_REFRESH_SECRET || '',
-        jwtAccessSecret: process.env.JWT_ACCESS_SECRET || '',
+        url: process.env.URL ?? 'http://localhost:3123',
+        port: parseInt(process.env.PORT ?? '3123'),
+        uiUrl: process.env.UI_URL ?? '',
+        dataDir: process.env.DATA_DIR ?? '',
+        jwtIssuer: process.env.JWT_ISSUER ?? '',
+        jwtAudience: process.env.JWT_AUDIENCE ?? '',
+        jwtRefreshExpires: process.env.JWT_REFRESH_EXPIRES ?? '',
+        jwtAccessExpires: process.env.JWT_ACCESS_EXPIRES ?? '',
+        jwtRefreshSecret: process.env.JWT_REFRESH_SECRET ?? '',
+        jwtAccessSecret: process.env.JWT_ACCESS_SECRET ?? '',
     };
 };

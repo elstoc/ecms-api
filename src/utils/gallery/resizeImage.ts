af5291fc45a6cbce9c4bf7e67f9b9c3848b70ec5
Chris Elston <chris.elston@blueyonder.co.uk>
2023-11-12 17:30:36 +0000

    Even lighter border

--- src/utils/gallery/resizeImage.ts
+++ src/utils/gallery/resizeImage.ts
@@ -18,7 +18,7 @@ export const resizeImage = (sourceImage: Buffer, config: ResizeConfig): Promise<
             .resize(width, height)
             .quality(quality);
         
-        if (addBorder) resizedImage = resizedImage.borderColor('rgb(40,40,40)').border(2, 2);
+        if (addBorder) resizedImage = resizedImage.borderColor('rgb(50,50,50)').border(2, 2);
         if (stripExif) resizedImage = resizedImage.strip();
 
         return resizedImage.toBuffer(

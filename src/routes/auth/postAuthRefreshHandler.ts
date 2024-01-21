2abcfa08427cdd00fe414558d21dc9057d872b9a
Chris Elston <chris.elston@blueyonder.co.uk>
2024-01-21 17:45:30 +0000

    Set maxAge and sameSite for refresh cookie

--- src/routes/auth/postAuthRefreshHandler.ts
+++ src/routes/auth/postAuthRefreshHandler.ts
@@ -8,7 +8,9 @@ export const createPostAuthRefreshHandler = (auth: IAuth, logger: winston.Logger
         const currentRefreshToken = req.cookies?.refresh_token;
         const tokens = await auth.getTokensFromRefreshToken(currentRefreshToken);
         const { accessToken, accessTokenExpiry, refreshToken } = tokens;
-        res.cookie('refresh_token', refreshToken, { httpOnly: true });
+        // TODO: set max age to the same as the refresh expiry
+        const maxAge = 60 * 60 * 24 * 365; // 1 year
+        res.cookie('refresh_token', refreshToken, { httpOnly: true, maxAge, sameSite: true });
         res.json({ accessToken, accessTokenExpiry}).status(200);
     } catch (e: unknown) {
         if (e instanceof Error) {

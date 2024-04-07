29783455022397d14f00067ced718e22e90b1950
Chris Elston <chris.elston@blueyonder.co.uk>
2024-04-07 22:34:29 +0100

    Fix authentication error

--- src/utils/auth/jwt.ts
+++ src/utils/auth/jwt.ts
@@ -1,9 +1,10 @@
 import jwt from 'jsonwebtoken';
+import { AuthenticationError } from '../../errors';
 
 export const jwtSign = async (payload: object, secret: string, expiresIn: string | number): Promise<string | undefined> => {
     return new Promise((resolve, reject) => {
         jwt.sign(payload, secret, { expiresIn }, (err, token) => {
-            if (err) reject(err);
+            if (err) reject(new AuthenticationError(err.message));
             resolve(token);
         });
     });
@@ -12,7 +13,7 @@ export const jwtSign = async (payload: object, secret: string, expiresIn: string
 export const jwtVerify = async (token: string, secret: string): Promise<string | jwt.JwtPayload | undefined> => {
     return new Promise((resolve, reject) => {
         jwt.verify(token, secret, (err, decoded) => {
-            if (err) reject(err);
+            if (err) reject(new AuthenticationError(err.message));
             resolve(decoded);
         });
     });

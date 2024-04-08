38603d79626053409b9c47c5c985d1c863e831b7
Chris Elston <chris.elston@blueyonder.co.uk>
2024-04-08 21:15:03 +0100

    Update utils index

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export { Config, getConfig } from './config';
 export { splitPath } from './splitPath';
 export { sortByWeightAndTitle } from './sortByWeightAndTitle';
+export { convertToRecord, convertToStringArray, getRecordAtPath, isEmpty} from './objectUtils';

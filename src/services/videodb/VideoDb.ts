fc8202bb615d5acfe30d52380f06143246bb0fc8
Chris Elston <chris.elston@blueyonder.co.uk>
2025-01-26 16:39:31 +0000

    VideoDb: Lift shared datatypes to top level

--- src/services/videodb/VideoDb.ts
+++ src/services/videodb/VideoDb.ts
@@ -8,31 +8,7 @@ import { NotFoundError, NotPermittedError } from '../../errors';
 import { Config } from '../../utils';
 import { userIsAdmin } from '../auth/utils/access';
 import { User } from '../../contract/auth.contract';
-
-export type Video = {
-    title: string;
-    category: string;
-    director: string | null;
-    num_episodes: number | null;
-    length_mins: number | null;
-    watched: string;
-    priority_flag: number | null;
-    progress: string | null;
-    year: number | null;
-    imdb_id: string | null;
-    image_url: string | null;
-    actors: string | null;
-    plot: string | null;
-    tags: string | null;
-    primary_media_type: string | null;
-    primary_media_location: string | null;
-    primary_media_watched: string | null;
-    other_media_type: string | null;
-    other_media_location: string | null;
-    media_notes: string | null;
-}
-
-export type VideoWithId = Video & { id: number; };
+import { Video, VideoUpdate, VideoWithId } from '../../contract/videodb.contract';
 
 export const videoFields = [
     'title', 'category', 'director', 'num_episodes', 'length_mins', 'watched', 'priority_flag', 'progress',
@@ -69,10 +45,6 @@ export type VideoFilters = {
     minResolution?: string;
 }
 
-export type VideoUpdate = {
-    id: number;
-    priority_flag: 0 | 1;
-}
 const wait = (timeMs: number) => new Promise(resolve => setTimeout(resolve, timeMs));
 
 export class VideoDb {
